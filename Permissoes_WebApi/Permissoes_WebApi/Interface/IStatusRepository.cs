77df52b1582efe502af1ad88aae8a284b341e7e2
João Nascimento <jhonyzito4gb@gmail.com>
2022-08-04 20:54:08 -0300

    Anotações

--- Permissoes_WebApi/Permissoes_WebApi/Interface/IStatusRepository.cs
+++ Permissoes_WebApi/Permissoes_WebApi/Interface/IStatusRepository.cs
@@ -8,25 +8,25 @@ namespace Permissoes_WebApi.Interface
         List<Status> Listar();
 
         /// <summary>
-        /// método utilizado para buscr um usuário existente
+        /// método utilizado para buscar um status existente
         /// </summary>
-        /// <param name="id">id do usuário que irá passar pela url da requisição</param>
+        /// <param name="id">id do status que irá passar pela url da requisição</param>
         /// <returns>usuário buscado</returns>
         Status BuscarPorId(int id);
 
         /// <summary>
-        /// método utilizado para cadastrar um novo usuário
+        /// método utilizado para cadastrar um novo status
         /// </summary>
-        /// <param name="NovoUsuario">objeto que irá armazenar as informações do novo usuário cadastrado</param>
+        /// <param name="novoStatus">objeto que irá armazenar as informações do novo status cadastrado</param>
         void Cadastro(Status novoStatus);
 
         void Deletar(int id);
 
         /// <summary>
-        /// método utilizado para atualizar determinado usuário
+        /// método utilizado para atualizar determinado status
         /// </summary>
-        /// <param name="id">id do usuário buscado</param>
-        /// <param name="usuario">objeto que irá armazenar as novas informações do usuário</param>
+        /// <param name="id">id do status buscado</param>
+        /// <param name="novoStatus">objeto que irá armazenar as novas informações do status</param>
         void AtualizarStatus(int id, Status novoStatus);
     }
 }
